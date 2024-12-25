77d70c61fa971117fea7c6cdb460c7371a91359a
x6mode <x6modee@gmail.com>
2024-12-26 00:22:13 +0300

    readme check list

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { Logger } from "@nestjs/common";
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3001);
